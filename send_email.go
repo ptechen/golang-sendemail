02223bff0b9ab38634f4c46269ecb798aca1c25d
chen <540123685@qq.com>
2019-01-23 22:42:05 +0800

    Update send_email.go

--- send_email.go
+++ send_email.go
@@ -24,7 +24,8 @@ type SendEmailParams struct {
 	ContentType string  //"Content-Type: text/plain; charset=UTF-8"
 } 
 
-func (self *SendEmailParams) SendEmail(sendMsg, subject, nickName string, send2Email []string) (err error) {
+//通过未加密的连接发送电子邮件
+func (self *SendEmailParams) UnencryptedSendEmail(sendMsg, subject, nickName string, send2Email []string) (err error) {
 	auth := &unencryptedAuth{
 		smtp.PlainAuth("", self.UserEmail, self.Password, self.Host),
 	}
@@ -35,3 +36,15 @@ func (self *SendEmailParams) SendEmail(sendMsg, subject, nickName string, send2E
 	err = smtp.SendMail(address, auth, self.UserEmail, send2Email, msgByte)
 	return
 }
+
+//通过加密的连接发送电子邮件
+func (self *SendEmailParams) SendEmail(sendMsg, subject, nickName string, send2Email []string) (err error) {
+	auth := smtp.PlainAuth("", self.UserEmail, self.Password, self.Host)
+	msgStr := fmt.Sprintf("To: %s \r\nFrom: %s <%s>\r\nSubject: %s\r\n%s\r\n\r\n%s",
+		strings.Join(send2Email, ","), nickName, self.UserEmail, subject, self.ContentType, sendMsg)
+	msgByte := []byte(msgStr)
+	address := fmt.Sprintf("%s:%d", self.Host, self.Port)
+	err = smtp.SendMail(address, auth, self.UserEmail, send2Email, msgByte)
+	return
+}
+
